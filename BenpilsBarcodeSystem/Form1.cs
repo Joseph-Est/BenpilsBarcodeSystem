6aa445983d6b330f77738296703531c88e280929
Joseph-Est <josephesteban13@yahoo.com>
2023-09-26 19:45:19 +0800

    Update login icon

--- BenpilsBarcodeSystem/Form1.cs
+++ BenpilsBarcodeSystem/Form1.cs
@@ -75,5 +75,10 @@ namespace BenpilsBarcodeSystem
         {
             this.WindowState = FormWindowState.Minimized;
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

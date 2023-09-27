9eda50ad88eaef7259cc5e618f34627d3a72ae2a
Joseph Esteban <esteban.144092@ortigas-cainta.sti.edu.ph>
2023-09-27 14:45:23 +0800

    Update

--- BenpilsBarcodeSystem/Form1.cs
+++ BenpilsBarcodeSystem/Form1.cs
@@ -35,7 +35,7 @@ namespace BenpilsBarcodeSystem
             {
                 try
                 {
-                    SqlConnection con = new SqlConnection("Data Source=DESKTOP-D6B01T4;Initial Catalog=UserLogin;Integrated Security=True");
+                    SqlConnection con = new SqlConnection("Data Source=DESKTOP-GM16NRU;Initial Catalog=UserLoginn;Integrated Security=True");
                     SqlCommand cmd = new SqlCommand("select * from tbl_login where username = @username and password = @password", con);
                     cmd.Parameters.AddWithValue("@username", textBox1.Text);
                     cmd.Parameters.AddWithValue("@password", textBox2.Text);

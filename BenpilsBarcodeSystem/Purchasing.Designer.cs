6d0444a944213071f333dfbb726c2df651e0f314
Joseph-Est <josephesteban13@yahoo.com>
2023-10-07 19:42:30 +0800

    Purchasing

--- BenpilsBarcodeSystem/Purchasing.Designer.cs
+++ BenpilsBarcodeSystem/Purchasing.Designer.cs
@@ -121,7 +121,7 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(581, 7);
+            this.label5.Location = new System.Drawing.Point(497, 3);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(441, 20);
             this.label5.TabIndex = 10;

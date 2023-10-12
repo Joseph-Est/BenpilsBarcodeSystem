501a50dbb15cca438d1479286cbd36b39425140c
Jerico Abordo <abordo.110423@ortigas-cainta.sti.edu.ph>
2023-10-12 21:11:20 +0800

    reports ui

--- BenpilsBarcodeSystem/Reports.Designer.cs
+++ BenpilsBarcodeSystem/Reports.Designer.cs
@@ -126,7 +126,7 @@
             this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel5.Location = new System.Drawing.Point(182, 852);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(1374, 35);
+            this.panel5.Size = new System.Drawing.Size(1352, 35);
             this.panel5.TabIndex = 22;
             // 
             // label4
@@ -136,7 +136,7 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(1228, 8);
+            this.label4.Location = new System.Drawing.Point(1206, 8);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(45, 18);
             this.label4.TabIndex = 3;
@@ -395,14 +395,14 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1556, 35);
+            this.panel1.Size = new System.Drawing.Size(1534, 35);
             this.panel1.TabIndex = 19;
             // 
             // CloseBtn
             // 
             this.CloseBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.CloseBtn.Image = ((System.Drawing.Image)(resources.GetObject("CloseBtn.Image")));
-            this.CloseBtn.Location = new System.Drawing.Point(1513, 3);
+            this.CloseBtn.Location = new System.Drawing.Point(1491, 3);
             this.CloseBtn.Name = "CloseBtn";
             this.CloseBtn.Size = new System.Drawing.Size(31, 32);
             this.CloseBtn.TabIndex = 22;
@@ -427,7 +427,7 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(564, 3);
+            this.label5.Location = new System.Drawing.Point(553, 3);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(441, 20);
             this.label5.TabIndex = 10;
@@ -437,7 +437,7 @@
             // 
             this.MinimizedBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.MinimizedBtn.Image = ((System.Drawing.Image)(resources.GetObject("MinimizedBtn.Image")));
-            this.MinimizedBtn.Location = new System.Drawing.Point(1469, 3);
+            this.MinimizedBtn.Location = new System.Drawing.Point(1447, 3);
             this.MinimizedBtn.Name = "MinimizedBtn";
             this.MinimizedBtn.Size = new System.Drawing.Size(38, 32);
             this.MinimizedBtn.TabIndex = 8;
@@ -458,7 +458,7 @@
             this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel6.Location = new System.Drawing.Point(182, 35);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(1374, 36);
+            this.panel6.Size = new System.Drawing.Size(1352, 36);
             this.panel6.TabIndex = 23;
             // 
             // label7
@@ -468,7 +468,7 @@
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(1228, 6);
+            this.label7.Location = new System.Drawing.Point(1206, 6);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(43, 18);
             this.label7.TabIndex = 2;
@@ -481,7 +481,7 @@
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(601, 6);
+            this.label8.Location = new System.Drawing.Point(590, 6);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(90, 18);
             this.label8.TabIndex = 1;
@@ -504,7 +504,7 @@
             this.panel3.Controls.Add(this.tabControl1);
             this.panel3.Location = new System.Drawing.Point(189, 78);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1355, 746);
+            this.panel3.Size = new System.Drawing.Size(1338, 746);
             this.panel3.TabIndex = 24;
             // 
             // tabControl1
@@ -515,7 +515,7 @@
             this.tabControl1.Location = new System.Drawing.Point(3, 3);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1349, 740);
+            this.tabControl1.Size = new System.Drawing.Size(1330, 740);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -523,7 +523,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1341, 714);
+            this.tabPage1.Size = new System.Drawing.Size(1322, 714);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Iventory Report";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -533,7 +533,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(1322, 714);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Sales Report";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -543,7 +543,7 @@
             this.tabPage3.Controls.Add(this.tabControl2);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1341, 714);
+            this.tabPage3.Size = new System.Drawing.Size(1322, 714);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Audit Trail";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -556,7 +556,7 @@
             this.tabControl2.Location = new System.Drawing.Point(0, 21);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(1345, 697);
+            this.tabControl2.Size = new System.Drawing.Size(1322, 697);
             this.tabControl2.TabIndex = 0;
             // 
             // tabPage4
@@ -567,7 +567,7 @@
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(1337, 671);
+            this.tabPage4.Size = new System.Drawing.Size(1314, 671);
             this.tabPage4.TabIndex = 0;
             this.tabPage4.Text = "POS Audit Trail";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -580,7 +580,7 @@
             this.tabPage5.Location = new System.Drawing.Point(4, 22);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(1337, 671);
+            this.tabPage5.Size = new System.Drawing.Size(1314, 671);
             this.tabPage5.TabIndex = 1;
             this.tabPage5.Text = "Supplier Audit Trail";
             this.tabPage5.UseVisualStyleBackColor = true;
@@ -592,7 +592,7 @@
             this.tabPage6.Controls.Add(this.label2);
             this.tabPage6.Location = new System.Drawing.Point(4, 22);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(1337, 671);
+            this.tabPage6.Size = new System.Drawing.Size(1314, 671);
             this.tabPage6.TabIndex = 2;
             this.tabPage6.Text = "Inventory Audit Trail";
             this.tabPage6.UseVisualStyleBackColor = true;
@@ -621,7 +621,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(11, 61);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1308, 593);
+            this.dataGridView1.Size = new System.Drawing.Size(1287, 593);
             this.dataGridView1.TabIndex = 41;
             // 
             // dataGridView2
@@ -629,7 +629,7 @@
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Location = new System.Drawing.Point(16, 57);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(1308, 593);
+            this.dataGridView2.Size = new System.Drawing.Size(1283, 593);
             this.dataGridView2.TabIndex = 44;
             // 
             // textBox1
@@ -656,7 +656,7 @@
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView3.Location = new System.Drawing.Point(16, 57);
             this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.Size = new System.Drawing.Size(1308, 593);
+            this.dataGridView3.Size = new System.Drawing.Size(1282, 593);
             this.dataGridView3.TabIndex = 44;
             // 
             // textBox2
@@ -682,7 +682,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1556, 887);
+            this.ClientSize = new System.Drawing.Size(1534, 887);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel5);

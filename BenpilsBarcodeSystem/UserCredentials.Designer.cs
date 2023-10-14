36a4449583007047a74eed619ff5935463ff5790
Joseph-Est <josephesteban13@yahoo.com>
2023-10-14 19:19:05 +0800

    refresh button user cred

--- BenpilsBarcodeSystem/UserCredentials.Designer.cs
+++ BenpilsBarcodeSystem/UserCredentials.Designer.cs
@@ -962,7 +962,7 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.White;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1519, 73);
+            this.pictureBox2.Location = new System.Drawing.Point(1484, 73);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(40, 39);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;

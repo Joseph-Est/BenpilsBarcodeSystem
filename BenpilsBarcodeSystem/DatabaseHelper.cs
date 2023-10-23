c9f7817304fefda8864378024ae11733c0b98caa
Joseph-Est <josephesteban13@yahoo.com>
2023-10-23 20:04:12 +0800

    ComboBox database

---
+++ BenpilsBarcodeSystem/DatabaseHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenpilsBarcodeSystem
+{
+    public  class DatabaseHelper
+    {
+        private string connectionString = "Data Source=DESKTOP-GM16NRU;Initial Catalog=BenpillMotorcycleDatabase;Integrated Security=True";
+
+        public DataTable GetSupplierData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Modify this query according to your database structure and relationships.
+                string query = "SELECT SupplierID, ContactName FROM tbl_supplier";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+    }
+}

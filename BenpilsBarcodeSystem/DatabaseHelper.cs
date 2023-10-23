aee7ee09e5323e436cb825ef49f146cc228cce7b
Joseph Esteban <esteban.144092@ortigas-cainta.sti.edu.ph>
2023-10-23 20:12:42 +0800

    awdawdaw

---
+++ BenpilsBarcodeSystem/DatabaseHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenpilsBarcodeSystem
+{
+    public class DatabaseHelper
+    {
+        private string connectionString = "YourConnectionStringHere";
+
+        public DataTable GetSupplierData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Modify this query according to your database structure and relationships.
+                string query = "SELECT SupplierID, ContactName FROM Suppliers";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+    }
+}

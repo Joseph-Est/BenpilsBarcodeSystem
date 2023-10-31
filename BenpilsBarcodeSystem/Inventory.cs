e64bd17c536450bcc92003ed48a48e45fbdc23a5
Joseph Esteban <esteban.144092@ortigas-cainta.sti.edu.ph>
2023-10-31 15:39:59 +0800

    Inventory Column adjust

--- BenpilsBarcodeSystem/Inventory.cs
+++ BenpilsBarcodeSystem/Inventory.cs
@@ -145,6 +145,8 @@ namespace BenpilsBarcodeSystem
        
         private void Inventory_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'benpillMotorcycleItemmasterDataMain.tbl_itemmasterdata' table. You can move, or remove it, as needed.
+            this.tbl_itemmasterdataTableAdapter1.Fill(this.benpillMotorcycleItemmasterDataMain.tbl_itemmasterdata);
             // TODO: This line of code loads data into the 'benpillMotorcycleMasterData.tbl_itemmasterdata' table. You can move, or remove it, as needed.
             this.tbl_itemmasterdataTableAdapter.Fill(this.benpillMotorcycleMasterData.tbl_itemmasterdata);
 

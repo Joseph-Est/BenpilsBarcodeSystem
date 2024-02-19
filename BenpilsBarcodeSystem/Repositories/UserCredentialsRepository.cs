67af7b325b9d898d5ddd9119c68c07adf35fe533
John Mark Payas <sklerbidi.09@gmail.com>
2024-02-19 21:06:18 +0800

    update 0.05 - UserCredentialsRepository

--- BenpilsBarcodeSystem/Repositories/UserCredentialsRepository.cs
+++ BenpilsBarcodeSystem/Repositories/UserCredentialsRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,29 @@ namespace BenpilsBarcodeSystem.Repository
         public UserCredentialsRepository()
         {
             databaseConnection = new Database.DatabaseConnection();
+        }
+
+        public async Task<DataTable> GetUserCredentialsAsync()
+        {
+            string selectQuery = "SELECT * FROM tbl_usercredential";
+
+            try
+            {
+                using (SqlConnection con = databaseConnection.OpenConnection())
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(selectQuery, con))
+                    {
+                        DataTable dt = new DataTable();
+                        await Task.Run(() => adapter.Fill(dt));
+                        return dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                return null;
+            }
         }
     }
 }
